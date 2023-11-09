d3d1d66a312eaf44f2e10a2a7c2fa8f71e4381bf
heliyuan <heliyuan@51xf.cn>
2023-11-09 10:30:02 +0800

    代码更新

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+DATABASE = 'data_science_platform.db'
+
+# 假定的数据项，实际开发中这可能是数据库查询的结果
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row  # This enables column access by name: row['column_name']
+    return conn
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/data')
+def get_data():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM data_items')  # Replace 'your_data_table' with your table name
+    data_items = cur.fetchall()
+    # Convert query to dictionary
+    data_dicts = [dict(row) for row in data_items]
+    conn.close()
+    return jsonify(data_dicts)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,3 +2,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     // 我们可以添加更多的JavaScript代码来处理页面动态功能
 });
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取数据并在表格中展示
+    fetch('/api/data')
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.getElementById('data-table-body');
+            // 清空表格
+            tableBody.innerHTML = '';
+            // 为每个数据项创建表格行
+            data.forEach(item => {
+                const row = `<tr>
+                                <td>${item.id}</td>
+                                <td>${item.name}</td>
+                                <td>${item.description}</td>
+                             </tr>`;
+                tableBody.innerHTML += row;
+            });
+        });
+});
