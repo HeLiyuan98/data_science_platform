94f67e88c5a909c0a068e6dfe8c3b10f7161118e
heliyuan <heliyuan@51xf.cn>
2023-11-11 21:48:26 +0800

    更新代码

--- app.py
+++ app.py
@@ -1,30 +1,87 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import sqlite3
+import base64
+import pandas as pd
+
 
 app = Flask(__name__)
 
-DATABASE = 'data_science_platform.db'
+DATABASE = 'pandas.db'
 
-# 假定的数据项，实际开发中这可能是数据库查询的结果
+
+
+# 建立与sqlite数据库的连接
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row  # This enables column access by name: row['column_name']
+    conn.row_factory = sqlite3.Row# This enables column access by name: row['column_name']
     return conn
 
+#将数据库行转换成字典
+def row_to_dict(row):
+    row_dict = {}
+    for key, value in dict(row).items():
+        if isinstance(value, bytes):
+            # 正确地将字节类型的数据转换为 Base64 编码的字符串
+            row_dict[key] = base64.b64encode(value).decode('utf-8')
+        else:
+            row_dict[key] = value
+    return row_dict
+
+
+#路由处理函数处理对/api/pandas 的 GET 请求。
+@app.route('/api/pandas')
+def get_pandas():
+    page = request.args.get('page', 1, type=int)
+    per_page = 8
+    name_query = request.args.get('name')
+    conn = get_db_connection()
+    query = "SELECT * FROM pandas_info"
+    params = []
+    if name_query:
+        query += f" WHERE name LIKE ?"
+        params.append(f"%{name_query}%")
+
+    query += " LIMIT ? OFFSET ?"
+    params.extend([per_page, (page - 1) * per_page])
+
+    pandas = conn.execute(query,params).fetchall()
+    conn.close()
+
+    pandas_data = [row_to_dict(row) for row in pandas]
+    return jsonify(pandas_data)
+
+#根据特定的条件从数据库中检索信息，结果以字典的形式返回
+def get_panda_info(panda_id=None, name=None):
+    conn = sqlite3.connect('pandas.db')
+    query = "SELECT * FROM pandas_info"
+    conditions = []
+    if panda_id:
+        conditions.append(f"id={panda_id}")
+    if name:
+        conditions.append(f"name LIKE '%{name}%'")
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df.to_dict(orient='records')
+
+#返回主页的HTML内容
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/data')
-def get_data():
+#用于展示大熊猫的详细信息
+@app.route('/panda/<int:panda_id>')
+def panda_details(panda_id):
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM data_items')  # Replace 'your_data_table' with your table name
-    data_items = cur.fetchall()
-    # Convert query to dictionary
-    data_dicts = [dict(row) for row in data_items]
+    panda_detail = conn.execute('SELECT * FROM pandas_detail_info WHERE id = ?', (panda_id,)).fetchone()
     conn.close()
-    return jsonify(data_dicts)
+    if panda_detail is None:
+        return render_template('error.html', message='Panda not found'), 404
+    else:
+        return render_template('detail.html', panda=row_to_dict(panda_detail))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,23 +1,86 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    // 我们可以添加更多的JavaScript代码来处理页面动态功能
-});
 document.addEventListener('DOMContentLoaded', function() {
-    // 获取数据并在表格中展示
-    fetch('/api/data')
+    fetchAndDisplayPandas();
+
+        const searchButton = document.getElementById('search-button');
+        searchButton.addEventListener('click', function() {
+        const query = document.getElementById('searchBox').value;
+        fetchAndDisplayPandas(query);
+        
+    });
+    const nextButton = document.getElementById('next-button');
+    nextButton.addEventListener('click', function() {
+        fetchAndDisplayPandas(currentQuery, currentPage + 1);
+    });
+    const prevButton = document.getElementById('prev-button');
+    prevButton.addEventListener('click', function() {
+        if (currentPage>1){  //只有不在第一页的时候才可以向前翻页
+            fetchAndDisplayPandas(currentQuery, currentPage - 1);
+        }
+    });
+});
+
+let currentPage = 1; // 当前页码的全局变量
+let currentQuery = ''; // 添加一个变量来保存当前查询
+
+function fetchAndDisplayPandas(query='', page=1) {
+    // 更新当前页码
+    currentPage = page;
+    currentQuery = query; // 更新当前查询
+    // 构建带有查询参数的URL
+    const url = `/api/pandas?name=${encodeURIComponent(query)}&page=${currentPage}`;
+    
+    fetch(url)
+    .then(response => response.json())
+        .then(data => {
+            displayPandaCards(data); // 确保这里调用了 displayPandaCards
+            updatePaginationControls(data); // 确保这里调用了 updatePaginationControls，并且传递了 data
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function updatePaginationControls(data) {
+    // 更新页面上的分页控件，例如启用或禁用“上一页”和“下一页”按钮
+    // 这将依赖于你页面上的具体实现
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+    
+    // 逻辑来决定何时启用或禁用分页按钮
+    // 假设我们不知道总页数，我们可以在没有数据返回时禁用“下一页”按钮
+    // 如果我们在返回的数据中有总页数信息，我们可以使用它来判断
+    if (prevButton && nextButton) {
+        prevButton.disabled = currentPage <= 1;
+        nextButton.disabled = data.length < 8; // 使用传递的 data 参数
+    } else {
+        console.error('Pagination buttons not found');
+    }
+}
+
+
+function displayPandaCards(pandas) {
+    const container = document.getElementById('pandas-container');
+    container.innerHTML = ''; // 清除旧内容
+
+    pandas.forEach(panda => {
+        const card = document.createElement('div');
+        card.className = 'panda-card';
+        card.innerHTML = `
+            <a href="/panda/${panda.id}">${panda.name}</a>
+            <a href="/panda/${panda.id}">
+                <img src="${panda.image_url}" alt="${panda.name}" />
+            </a>
+            <p>性别: ${panda.gender}</p>
+            <p>年龄: ${panda.age}</p>
+        `;
+        container.appendChild(card);
+    });
+}
+
+function fetchPandaDetails(pandaId) {
+    // 这里发送请求到后端以获取熊猫的详细信息
+    fetch(`/api/panda/${pandaId}`)
         .then(response => response.json())
         .then(data => {
-            const tableBody = document.getElementById('data-table-body');
-            // 清空表格
-            tableBody.innerHTML = '';
-            // 为每个数据项创建表格行
-            data.forEach(item => {
-                const row = `<tr>
-                                <td>${item.id}</td>
-                                <td>${item.name}</td>
-                                <td>${item.description}</td>
-                             </tr>`;
-                tableBody.innerHTML += row;
-            });
-        });
-});
+            displayPandaDetails(data); // 显示熊猫的详细信息
+        })
+        .catch(error => console.error('Error:', error));
+}
